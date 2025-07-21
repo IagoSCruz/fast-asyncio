4ac3b5109bcd0c75432b359a222804a81f1d37f7
Iago Cruz <iagosilvacontato@gmail.com>
2025-07-20 22:54:11 -0300

    Refact: included response methods in app.py

--- fast_asyncio/app.py
+++ fast_asyncio/app.py
@@ -1,8 +1,16 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from fast_asyncio.schemas import Message, UserPublic, UserSchema
+
+app = FastAPI(title='API de Cadastros')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, mundo!'}
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
